f4c5ba920153fc3845e7c0a26f1b8d55292a6d6b
PolinaBY <112944644+PolinaBY@users.noreply.github.com>
2022-11-16 19:20:13 +0300

    Create forma.js

---
+++ forma.js
@@ -0,0 +1,77 @@
+let button = document.querySelector('button');
+let form = document.querySelector('.formaa');
+let popup = document.querySelector('.popup');
+
+let names = document.getElementById('name');
+let email = document.getElementById('email');
+let sms = document.getElementById('sms');
+let checkbox = document.getElementById('checkbox');
+
+function save() {
+  localStorage.setItem('Имя', names.value);
+  localStorage.setItem('Эл. почта', email.value);
+  localStorage.setItem('Сообщение', sms.value);
+  if (checkbox.checked) {
+    localStorage.setItem('Согласие', 1);
+  } else {
+    localStorage.setItem('Согласие', 0);
+  }
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  names.value = localStorage.getItem('Имя');
+  email.value = localStorage.getItem('Эл. почта');
+  sms.value = localStorage.getItem('Сообщение');
+  let checkBox = localStorage.getItem('Согласие');
+  if (checkBox == 1) {
+    checkbox.checked = true;
+  } else if (checkBox == 0) {
+    checkbox.checked = false;
+  }
+  
+  names.oninput = save;
+  email.oninput = save;
+  sms.oninput = save;
+  checkbox.oninput = save;
+
+  button.addEventListener('click', (event) => {
+    event.preventDefault();
+    window.onpopstate = function () {
+      window.history.back();
+      form.classList.remove('open');
+      popup.classList.remove('popup_open');
+    };
+    history.pushState({ page: 1 }, '', '?forma');
+    form.classList.add('open');
+    popup.classList.add('popup_open');
+  });
+
+  $(function(){
+    $(".formaa").submit(function(e){
+      e.preventDefault();
+      var href = $(this).attr("action");
+      $.ajax({
+          type: "POST",
+          dataType: "json",
+          url: href,
+          data: $(this).serialize(),
+          success: function(response){
+              if(response.status == "success"){
+                  alert("Форма отправленна успешно!");
+                  localStorage.removeItem('Имя');
+                  localStorage.removeItem('Эл. почта');
+                  localStorage.removeItem('Сообщение');
+                  localStorage.removeItem('Согласие');
+                  names.value = localStorage.getItem('Имя');
+                  email.value = localStorage.getItem('Эл. почта');
+                  sms.value = localStorage.getItem('Сообщение');
+                  checkbox.checked = false;
+              }
+          },
+          error: function (status) {
+            alert('Ошибка!');
+          },
+      });
+    });
+  });
+}); 
